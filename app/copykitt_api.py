bc838b96d00cc89b9c1e54b60511df882c1987de
Hayrol Reyes <hayrol@eamrfid.com>
2024-09-26 04:11:10 -0600

    Deploy to AWS Lambda

--- app/copykitt_api.py
+++ app/copykitt_api.py
@@ -1,7 +1,11 @@
 from fastapi import FastAPI, HTTPException
 from copykitt import generate_branding_snippet, generate_branding_keywords, validate_subject, MAX_INPUT_LENGTH
+from mangum import Mangum
+
 
 app = FastAPI()
+handler = Mangum(app)
+
 
 @app.get("/")
 async def read_root():
@@ -13,11 +17,13 @@ async def read_root():
             "documentation": "../docs"
             }
 
+
 @app.get("/gen-snippets")
 async def generate_snippets(subject: str):
     validate_input_length(subject)
     snippets = generate_branding_snippet(subject)
     return {"snippets": snippets, "keywords": None}
+
 
 @app.get("/gen-keywords")
 async def generate_keywords(subject: str):
@@ -25,12 +31,14 @@ async def generate_keywords(subject: str):
     keywords = generate_branding_keywords(subject)
     return {"snippets": None, "keywords": keywords}
 
+
 @app.get("/gen-snippets-and-keywords")
 async def generate_snippets(subject: str):
     validate_input_length(subject)
     snippets = generate_branding_snippet(subject)
     keywords = generate_branding_keywords(subject)
     return {"snippets": snippets, "keywords": keywords}
+
 
 def validate_input_length(subject: str):
     if not validate_subject(subject):
